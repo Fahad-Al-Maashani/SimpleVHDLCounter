e04f9064b8edd5c7df3155079d9e92c1a730322a
Fahad-Al-Maashani <122757915+Fahad-Al-Maashani@users.noreply.github.com>
2024-10-28 09:22:06 +0400

    Update Counter.vhdl
    debugging and fixing some syntax error

--- Counter.vhdl
+++ Counter.vhdl
@@ -1,24 +1,27 @@
--- This simple vhdl code for writing a simple led counter
+-- Simple Vhdl Code shows how counter work.
+-- as loong clk positive edge counter will be incremented otherwise adjusting clock speed by rst.
 library IEEE;
-use IEEE.std_logic_1164.all;
-entity WorldView is 
-port (
-clock : in std_logic;
-output: out std_logic;
-);
+use IEEE.STD_LOGIC_1164.ALL;
 
+entity WorldView is
+    port (
+        clock : in std_logic;
+        output : out std_logic
+    );
 end WorldView;
 
-architecture behavioral of WorldView is 
-signal count : integer := 0;
-begin 
-process (clock) 
-begin 
-if  rising_edge(clock) then count <= count +1;
-if count = 50000000 then -- Adjust for your clock speed
-output <= not output; 
-count <= 0;
-end if;
-end if;
-end process; 
-end behavioral; 
+architecture Behavioral of WorldView is
+    signal count : integer := 0;
+begin
+    process(clock)
+    begin
+        if rising_edge(clock) then
+            count <= count + 1;
+            if count = 50000000 then -- Adjust for your clock speed
+                output <= not output;
+                count <= 0;
+            end if;
+        end if;
+    end process;
+end Behavioral;
+
